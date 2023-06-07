5bdc689a7be332397547bf5f03790249db746a30
Swgonet <132945957+Swgonet@users.noreply.github.com>
2023-06-07 16:21:33 +0300

    Update goit-homework-03.py

--- goit-homework-03.py
+++ goit-homework-03.py
@@ -1,61 +1,51 @@
-di = {'3': 45, 'didi': 123}
-
+contacts = {}
 
 def input_error(func):
     def inner(*args,**kwargs):
         try:
             func(*args,**kwargs)
-        except:
-            print("Try again")
-        finally:
-            return main()
+        except IndexError:
+            print("Please enter norm number")
+        except KeyError:
+            print("Enter user name")
+        except ValueError:
+            print("Give me name and phone please")
+    return inner
 
+@input_error
+def add_contact(press_str):
+    press_str = press_str.split()
+    contacts.update({press_str[1]: press_str[2]})
 
-def hello(x):
-    print("How can I help you?")
-    return main()
+@input_error
+def change_contact(press_str):
+    press_str = press_str.split()
+    contacts[press_str[1]] = press_str[2]
 
-@input_error(func)
-def add(x):
-    add = x
-    add = add.split()
-    di.update({add[0]: add[1]})
-    return main()
+@input_error
+def phone_contact(press_str):
+    press_str = press_str.split()
+    print(contacts[press_str[1]])
 
-@input_error(func)
-def change(x):
-    key = x
-    key = key.split()
-    di[key[0]] = key[1]
-    return main()
-
-@input_error(func)
-def phone(x):
-    print(di[x])
-    return main()
-
-def show_all():
-    for k,v in di.items():
+def show_all_contacts():
+    for k,v in contacts.items():
         print(k, v)
 
 def main():
     while True:
-        x = input()
-        x = x.lower()
-        if x == 'hello':
-            print(hello(x))        
-        if 'add ...' in x:
-            x = input()
-            add(x)    
-        if "change ..." in x:
-            x = input()
-            change(x)
-        if "phone ..." in x:
-            phone(x)    
-        if "show all" in x:
-            show_all()    
-        if x == 'close' or x == 'goode bye' or x == 'exit':
+        press_str = input()
+        press_str = press_str.lower()
+        if press_str == 'hello':
+            print("How can I help you?")        
+        if 'add' in press_str:
+            add_contact(press_str)    
+        if "change" in press_str:
+            change_contact(press_str)
+        if "phone" in press_str:
+            phone_contact(press_str)    
+        if "show all" in press_str:
+            show_all_contacts()    
+        if press_str == 'close' or press_str == 'goode bye' or press_str == 'exit':
             return "Good bye!"
-            
-
+      
 print(main())
